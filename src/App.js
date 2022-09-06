f0ee58a0f1b95842d9449f1945e702b08996a9ea
jarb <blueciola@gmail.com>
2022-09-05 23:12:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Features, SectionWrapper, Download } from "./components";
+import assets from './assets';
+import styles from "./styles/Global";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <SectionWrapper
+        title='You own store of Nifty NFTs
+     Start selling and Growing'
+        description='Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace.Minuto 2:37'
+
+        showBtn
+        mockupImg={assets.homeHero}
+        banner='banner'
+
+      />
+
+      <SectionWrapper
+        title='Smart User Interface MarketPlace'
+        description='Experience a buttery UI of ProfNet NFT Marketplace . Smooth constant colors of a fluent UI design,'
+
+
+        mockupImg={assets.homeCards}
+        reverse
+
+
+      />
+
+      <Features />
+
+      <SectionWrapper
+        title='Deployment'
+        description="ProfNet usin  Expo which runs natively on all users devices.You can easily get your app into people's hands"
+
+        
+        mockupImg={assets.feature}
+        reverse
+
+      />
+
+      <SectionWrapper
+        title='Creative way to showcase the store'
+        description='The app contains two screens.The firts screen list all NFTs while the second one shows the details of a specific NFT.'
+
+
+        mockupImg={assets.mockup}
+        banner='banner0'
+
+
+      />
+
+      <Download/>
+
+      <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04'>
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made whit love {""}
+        <span className='bold'>JavaScript mastery by Jarb's</span></p>
+      </div>
+
+
+
+    </>
   );
 }
 
